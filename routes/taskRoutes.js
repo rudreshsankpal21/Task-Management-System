175094ce19a0a9e97a4ba0dc78f2b8980a13f068
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-29 16:48:41 +0530

    Added All Task Routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const taskRoutes = express.Router();
+
+// Create Task
+taskRoutes.post("/", createTask);
+
+// Get all tasks
+taskRoutes.get("/", getAllTasks);
+
+// Get task by id
+taskRoutes.get("/:id", getTaskById);
+
+// Update task
+taskRoutes.put("/:id", updateTask);
+
+// delete task
+taskRoutes.delete("/:id", deleteTask);
+
+module.exports = taskRoutes;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 // Connect to DB
 connectDB();
